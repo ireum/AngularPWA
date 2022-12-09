8c4b8f7a515375d8a47c0920bdc6c2abc75baab6
ireum <wi.sudarta@gmail.com>
2022-12-09 12:29:40 +0100

    offline data

--- angular-pwa/src/app/app.component.ts
+++ angular-pwa/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-pwa';
+  text!: string;
+  isOffline = !navigator.onLine;
+
+  @HostListener('window:offline', ['$event'])
+  onOffline(event: Event) {
+    this.isOffline = true;
+  }
+
+  @HostListener('window:online', ['$event'])
+  onOnline(event: Event) {
+    this.isOffline = false;
+  }
+
+  ngOnInit(): void {
+    if (this.isOffline) {
+      this.loadOfflineData();
+    } else {
+      this.text = 'this is live online data';
+      this.saveOfflineData();
+    }
+  }
+
+  saveOfflineData() {
+    const offlineData = {
+      text: 'this was saved when the app was connected to the internet',
+    }
+    localStorage.setItem('offlineData', JSON.stringify(offlineData));
+  }
+
+  loadOfflineData() {
+    this.text = JSON.parse(localStorage.getItem('offlineData') || '').text;
+  }
 }
